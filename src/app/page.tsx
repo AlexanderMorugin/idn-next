582bac6597f8a4c1e0137baa1c8f39bf1afc7f7f
Alexander Morugin <nobilis@bk.ru>
2025-02-01 02:24:54 +0300

    embla-2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 import { Metadata } from 'next';
 
 import { Heading } from '@/components';
+import EmblaCarousel from '@/components/EmblaCarousel';
+import React from 'react'
+
+
+import { EmblaOptionsType } from 'embla-carousel'
+
+
+
 // import { SwiperSlider } from '@/components/swiper-slider';
 // import { NewSwiper } from '@/components/new-swiper';
 // import { SlickStaticProps } from '@/components/slick-stati-props';
-import { EmblaSlider } from '@/components/embla-slider';
+// import { EmblaSlider } from '@/components/embla-slider';
 
 export const metadata: Metadata = {
   title: 'Наши услуги | IDN Next Slider',
   description: 'Тестовое задание IDN Next Slider',
 };
+
+const OPTIONS: EmblaOptionsType = { loop: true }
+const SLIDE_COUNT = 5
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 export default function Home() {
   return (
@@ -19,7 +31,8 @@ export default function Home() {
 
       {/* <NewSwiper /> */}
       {/* <SlickStaticProps /> */}
-      <EmblaSlider />
+
+      <EmblaCarousel slides={SLIDES} options={OPTIONS}/>
     </>
   );
 }
