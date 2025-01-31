ce388b24951bf4688cec3f289cab323755778f02
Alexander Morugin <nobilis@bk.ru>
2025-02-01 00:11:54 +0300

    try react-slick

--- src/components/new-swiper.tsx
+++ src/components/new-swiper.tsx
@@ -1,25 +1,86 @@
 'use client'
 
-import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Virtual } from 'swiper/modules';
+import Image from "next/image";
 
-import 'swiper/css';
-import 'swiper/css/virtual';
-import 'swiper/css/pagination';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useCardStore } from '../../store/use-card-store';
+import { useEffect } from 'react';
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 1279,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
 
 export const NewSwiper = () => {
-  const slides = Array.from({ length: 1000 }).map(
-    (el, index) => `Slide ${index + 1}`
-  );
+  const { cardItems, getCardItems } = useCardStore((state) => state);
+
+  useEffect(() => {
+    getCardItems();
+  }, [getCardItems]);
 
   return (
-    <Swiper modules={[Virtual]} spaceBetween={50} slidesPerView={3} virtual>
-      {slides.map((slideContent, index) => (
-        <SwiperSlide key={slideContent} virtualIndex={index}>
-          {slideContent}
-        </SwiperSlide>
-      ))}
-    </Swiper>
+<Slider {...settings}>
+      {cardItems.map((item, i) => {
+        return (
+          <aside key={i} className="p-4">
+                <div className='flex flex-col justify-between h-[212px] bg-card rounded-lg p-6 mt-[50px] cursor-grab md:mt-0 md:h-[256px]'>
+                  <div className='flex flex-row justify-between gap-6 h-full md:flex-col'>
+                    <Image
+                      src={item.imageUrl}
+                      alt='Картинка карточки'
+                      className='w-[60px] h-[60px] md:w-[80px] md:h-[80px]'
+                      width={80}
+                      height={80}
+                    />
+                    <p className='text-[24px]/[28px] md:text-[32px]/[36px]'>{item.title}</p>
+                  </div>
+            
+                  {/** Subtitle появляется только в мобильной версии */}
+                  <div className='text-[14px]/[20px] tracking-wide md:hidden'>
+                    {item.subtitle}
+                  </div>
+                </div>
+          </aside>
+        );
+      })}
+    </Slider>
   );
 };
